04f19bca12c450774f6a3bf0302689ca7184f321
shgopher <shgopher@gmail.com>
2024-09-06 00:01:26 +0800

    feat: 🎸 add

--- learn-r-by-examples.r
+++ learn-r-by-examples.r
@@ -1,11 +1,19 @@
 # 通过例子学习 R 语言
 
 # 1. hello world
-print("hello world")
+# 这是 R 语言的 hello world
+myString = "hello world"
+print(myString)
+
+"
+hello world
+"
 
 # 2. R 语言的变量和数据类型
-x <- 1
-print(x)
+a = 2 %/% 7 # 0
+b = 2 %% 7 # 2
+cat(a,b)
+print(a > b)
 
 
 
